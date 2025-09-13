5eba8f70320b819637a2f3e22fe359383d0706c1
Patryk Koryzna <patryk@koryzna.com>
2025-09-13 15:54:56 +0200

    fix missing #endif

--- src/modules/Modules.cpp
+++ src/modules/Modules.cpp
@@ -227,6 +227,7 @@ void setupModules()
         new SerialModule();
     }
 #endif
+#endif
 #ifdef ARCH_ESP32
     // Only run on an esp32 based device.
 #if defined(USE_SX1280) && !MESHTASTIC_EXCLUDE_AUDIO
