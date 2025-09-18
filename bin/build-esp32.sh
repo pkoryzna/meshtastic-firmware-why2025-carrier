2f9865ffc93810670abfef28c34950abc78eb9c3
Patryk Koryzna <patryk@koryzna.com>
2025-09-18 13:49:13 +0200

    add unmerged partitions to build artifacts (why2025 badge)

--- bin/build-esp32.sh
+++ bin/build-esp32.sh
@@ -5,8 +5,7 @@ set -e
 VERSION=`bin/buildinfo.py long`
 SHORT_VERSION=`bin/buildinfo.py short`
 
-BUILDDIR=.pio/build/$1
-OUTDIR=release
+OUTDIR=release/
 
 rm -f $OUTDIR/firmware*
 rm -r $OUTDIR/* || true
@@ -15,27 +14,39 @@ rm -r $OUTDIR/* || true
 platformio pkg install -e $1
 
 echo "Building for $1 with $PLATFORMIO_BUILD_FLAGS"
-rm -f $BUILDDIR/firmware*
+rm -f .pio/build/$1/firmware.*
 
 # The shell vars the build tool expects to find
 export APP_VERSION=$VERSION
 
 basename=firmware-$1-$VERSION
 
-pio run --environment $1 -t mtjson # -v
+pio run --environment $1 # -v
+SRCELF=.pio/build/$1/firmware.elf
+cp $SRCELF $OUTDIR/$basename.elf
 
-cp $BUILDDIR/$basename.elf $OUTDIR/$basename.elf
+if [ $1 == "why2025-carrier" ]; then
+  # Sorry, they must be flashed separately.
+  echo "Copying ESP32-C6 bin files and JSON manifest"
+  cp -v .pio/build/$1/*.bin $OUTDIR/
+  cp -v .pio/build/$1/*.json $OUTDIR/
+else
+  echo "Copying ESP32 bin file"
+  SRCBIN=.pio/build/$1/firmware.factory.bin
+  cp $SRCBIN $OUTDIR/$basename.bin
 
-echo "Copying ESP32 bin file"
-cp $BUILDDIR/$basename.factory.bin $OUTDIR/$basename.factory.bin
-
-echo "Copying ESP32 update bin file"
-cp $BUILDDIR/$basename.bin $OUTDIR/$basename.bin
-
-echo "Copying Filesystem for ESP32 targets"
-cp $BUILDDIR/littlefs-$1-$VERSION.bin $OUTDIR/littlefs-$1-$VERSION.bin
-cp bin/device-install.* $OUTDIR/
-cp bin/device-update.* $OUTDIR/
-
-echo "Copying manifest"
-cp $BUILDDIR/$basename.mt.json $OUTDIR/$basename.mt.json || true
+  echo "Copying ESP32 update bin file"
+  SRCBIN=.pio/build/$1/firmware.bin
+  cp $SRCBIN $OUTDIR/$basename-update.bin
+fi
+echo "Building Filesystem for ESP32 targets"
+# If you want to build the webui, uncomment the following lines
+# pio run --environment $1 -t buildfs
+# cp .pio/build/$1/littlefs.bin $OUTDIR/littlefswebui-$1-$VERSION.bin
+# # Remove webserver files from the filesystem and rebuild
+# ls -l data/static # Diagnostic list of files
+# rm -rf data/static
+pio run --environment $1 -t buildfs
+cp .pio/build/$1/littlefs.bin $OUTDIR/littlefs-$1-$VERSION.bin
+cp bin/device-install.* $OUTDIR
+cp bin/device-update.* $OUTDIR
